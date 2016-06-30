e11801839e84c8246f45e9776a03e603bb85983f
st1ny <stiny@stiny.org>
2016-06-29 18:58:55 -0700

    Code reformat

--- ai/main.js
+++ ai/main.js
@@ -5,33 +5,24 @@ module.exports.loop = function () {
 
     // Always place this memory cleaning code at the very top of your main loop!
 
-    for (var name in Memory.creeps) {
-        if (!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            // console.log('Clearing non-existing creep memory:', name);
+    for (var dead in Memory.creeps) {
+        if (!Game.creeps[dead]) {
+            delete Memory.creeps[dead];
         }
     }
 
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester'
-    );
-    // console.log('Harvesters: ' + harvesters.length);
-
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     if (harvesters.length < 4) {
-        var newName = Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
-        // console.log('Spawning new harvester: ' + newName);
+        Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
     }
 
-    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader'
-    );
-    // console.log('Updaters: ' + updaters.length);
-
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     if (upgraders.length < 4) {
-        var newName = Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader'});
-        // console.log('Spawning new upgrader: ' + newName);
+        Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader'});
     }
 
-    for (var name in Game.creeps) {
-        var creep = Game.creeps[name];
+    for (var c in Game.creeps) {
+        var creep = Game.creeps[c];
         if (creep.memory.role == 'harvester') {
             roleHarvester.run(creep);
         }
