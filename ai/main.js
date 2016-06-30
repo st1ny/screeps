a6b452404a998cfd628f7e361a5f5759d8a2328f
st1ny <stiny@stiny.org>
2016-06-29 19:16:37 -0700

    Actually use the builder

--- ai/main.js
+++ ai/main.js
@@ -1,5 +1,6 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
@@ -33,6 +34,9 @@ module.exports.loop = function () {
         }
         if (creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
 };
